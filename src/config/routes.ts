db2c6b885e6662c2792e5c4713d69108c0f0ffdf
Daniil Oberlev <denioberlev@gmail.com>
2025-10-30 05:01:18 +0300

    feat(config): made a common config for project

---
+++ src/config/routes.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  HOME: '/',
+  SERVICES: '/services',
+  ABOUT: '/about',
+  GALLERY: '/gallery',
+  BLOG: '/blog',
+  FAQ: '/FAQ',
+  CONTACT: '/contact',
+  CALCULATOR: '/calculator',
+} as const;
